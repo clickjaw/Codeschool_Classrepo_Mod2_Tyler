a67f8db702319ef97f79230da67bccec0f398e2a
DJ <epicmrjones@gmail.com>
2022-11-09 09:25:56 -0600

    LinkedList Solution Project Init
    Created project for Solution to Linked List Implementation

---
+++ LinkedList_Implementation_Solution/LinkedList_Implementation_Solution/OurLinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Implementation_Solution
+{
+	internal class OurLinkedList
+	{
+		internal static void Hello() {
+			Console.WriteLine("Hey");
+		}
+	}
+}
---
+++ LinkedList_Implementation_Solution/LinkedList_Implementation_Solution/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedList_Implementation_Solution;
+
+Console.WriteLine("Hello, World!");
+OurLinkedList.Hello();
